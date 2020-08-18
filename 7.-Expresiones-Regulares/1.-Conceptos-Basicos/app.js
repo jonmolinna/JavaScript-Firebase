667521bfefc284bc93a6c506583679ea92d34ef5
jonmolinna <jmolina2624@gmail.com>
2020-08-18 11:48:29 -0500

    Tu primer Exp. Regular

---
+++ 7.-Expresiones-Regulares/1.-Conceptos-Basicos/app.js
@@ -0,0 +1,8 @@
+// Creando un Expresion Regular
+var expresion1 = new RegExp('abc');
+var expresion2 = /abc/;
+
+var parrafo = /text1\/n texto2 /; // Para hacer un salto de linea
+
+
+// Prueba y evalua tus primeras expresiones regulares
