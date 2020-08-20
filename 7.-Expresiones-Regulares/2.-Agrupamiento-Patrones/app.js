228e1dcfb9fc923bce2dc6940dd4a0bd3f1e55ed
jonmolinna <jmolina2624@gmail.com>
2020-08-20 11:57:41 -0500

    Agrupamiento de Patrones

---
+++ 7.-Expresiones-Regulares/2.-Agrupamiento-Patrones/app.js
@@ -0,0 +1,6 @@
+// 1.-Patrones no sensibles a mayusculas y minusculas
+ var expresion1 = /Hola Mundo/i;
+    //console.log(expresion1.test('Hola mundo')); // Coincide con el patron => true
+
+
+// 2.-Aprende a agrupar patrones en subexpresiones
