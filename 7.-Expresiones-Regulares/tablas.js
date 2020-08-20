2063c8d01c5bd3ddf882da76fd84b682e1935781
jonmolinna <jmolina2624@gmail.com>
2020-08-20 11:58:27 -0500

    Espreciones Regulares

---
+++ 7.-Expresiones-Regulares/tablas.js
@@ -0,0 +1,14 @@
+/**
+ *      \d  =>  Cualquier Numero
+ *      \w  =>  Cualquier letra o numero
+ *      \s  =>  Cualquier espacio en blanco incluido tabs, salto de linea o similares
+ *      \D  =>  Cualquier caracter que no sea un numero
+ *      \W  =>  Cualquier caracter que no sea alfanumerico
+ *      \S  =>  Cualquier caracter que no sea un espacio en blanco
+ *      .   =>  Cualquier caracter excepto nueva lineas
+ *      ^   =>  Negacion
+ *      +   =>  Significa que el patron se va a repetir una o mas veces
+ *      *   =>  Significa que el patron se va a repetir una o mas veces, pero acepta que no este presente
+ *      ?   =>  Es un patron opcional que reemplaza un caracter
+ *      /i  =>  Patron no sensible a mayuscula y minuscula
+ */
