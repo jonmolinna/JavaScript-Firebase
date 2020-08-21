5aaaa3727a14380230be686d79b29ec5248718d2
jonmolinna <jmolina2624@gmail.com>
2020-08-21 11:47:18 -0500

    Agrupamiento de Patrones

--- 7.-Expresiones-Regulares/2.-Agrupamiento-Patrones/app.js
+++ 7.-Expresiones-Regulares/2.-Agrupamiento-Patrones/app.js
@@ -4,3 +4,12 @@
 
 
 // 2.-Aprende a agrupar patrones en subexpresiones
+var expresion2 = /Woo+(hoo+)+/i;
+    //console.log(expresion2.test('Wooohooohooooo')); // true
+
+
+// 3.-Coincidencias y grupos mediante exec y match
+var expresion3 = /\d+/.exec('Del año 1984')
+console.log(expresion3)
+
+console.log('Hasta 1994'.match(/\d+/));
