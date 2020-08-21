1ba11414b7c996095a2b8ec34afce2f2952cd5f9
jonmolinna <jmolina2624@gmail.com>
2020-08-21 11:51:19 -0500

    Expresiones Regulares

--- 7.-Expresiones-Regulares/tablas.js
+++ 7.-Expresiones-Regulares/tablas.js
@@ -6,9 +6,14 @@
  *      \W  =>  Cualquier caracter que no sea alfanumerico
  *      \S  =>  Cualquier caracter que no sea un espacio en blanco
  *      .   =>  Cualquier caracter excepto nueva lineas
- *      ^   =>  Negacion
+ *      [^]   =>  Negacion
  *      +   =>  Significa que el patron se va a repetir una o mas veces
  *      *   =>  Significa que el patron se va a repetir una o mas veces, pero acepta que no este presente
  *      ?   =>  Es un patron opcional que reemplaza un caracter
  *      /i  =>  Patron no sensible a mayuscula y minuscula
+ *      /\bpalabra\b/ => Limitador de palabras, osea el patron debe cumplir
+ *      ^   =>  Evalua el inicio de una cadena de texto
+ *      $   =>  Evalua el final de una cadena de texto
+ *      |   =>  o
+ *      /g   =>  Realiza busqueda de manera global
  */
