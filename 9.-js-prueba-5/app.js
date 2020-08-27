199dd6d922a0393cca74772a5619cf395f03a01b
jonmolinna <jmolina2624@gmail.com>
2020-08-27 11:06:29 -0500

    Ejercicios de 1 al 8

--- 9.-js-prueba-5/app.js
+++ 9.-js-prueba-5/app.js
@@ -74,14 +74,103 @@ const Pregunta4 = (texto="", veces=undefined) => {
     }
 }
 
-Pregunta4('Hola Mundo', 5)
+//Pregunta4('Hola Mundo', 5)
 
 
-/******************** Tarea para Casa *************************** */
-/*
-5) Programa una función que invierta las palabras de una cadena de texto, pe. miFuncion("Hola Mundo") devolverá "odnuM aloH".
-6) Programa una función para contar el número de veces que se repite una palabra en un texto largo, pe. miFuncion("hola mundo adios mundo", "mundo") devolverá 2.
-7) Programa una función que valide si una palabra o frase dada, es un palíndromo (que se lee igual en un sentido que en otro), pe. mifuncion("Salas") devolverá true.
-8) Programa una función que elimine cierto patrón de caracteres de un texto dado, pe. miFuncion("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz") devolverá  "1, 2, 3, 4 y 5.
+/******************** CHALLENGE 5 *************************** */
+/**
+ * 5) Programa una función que invierta las palabras de una cadena de texto,
+ * pe. miFuncion("Hola Mundo") devolverá "odnuM aloH".
+ */
+function Pregunta5(palabra=""){
+    if(!palabra){
+        console.warn("Ingresa una Cadena");
+    }
+    else {
+        console.info(palabra.split("").reverse().join(""));
+    }
+}
 
-*/
+//Pregunta5("Hola Mundo")
+
+
+/******************** CHALLENGE 6 *************************** */
+/**
+ * 6) Programa una función para contar el número de veces que se repite una palabra
+ * en un texto largo, pe. miFuncion("hola mundo adios mundo", "mundo") devolverá 2.
+ */
+function Pregunta6(cadena="", texto=""){
+    if(!cadena) return console.warn("Ingresa un texto largo");
+    if(!texto) return console.warn("Ingresa una palabra a evaluar");
+
+    let i=0;
+    let contador=0;
+
+    while(i !== -1){
+        i = cadena.indexOf(texto, i);
+        if(i !== -1){
+            i++;
+            contador++;
+        }
+    }
+    return console.info(`La palabra se repite ${contador}`);
+}
+
+//Pregunta6("hola mundo adios mundo", "mundo");
+
+
+/******************** CHALLENGE 7 *************************** */
+/**
+ * 7) Programa una función que valide si una palabra o frase dada, es un palíndromo
+ * (que se lee igual en un sentido que en otro), pe. mifuncion("Salas") devolverá true.
+ */
+function Pregunta7(palabra){
+    if(!palabra) return console.warn("Ingresa una Palabra");
+
+    palabra = palabra.toLowerCase();
+    let alReves = palabra.split("").reverse().join("");
+
+    return palabra === alReves;
+}
+
+//const output =Pregunta7("Salas")
+//console.log(output)
+
+
+/******************** CHALLENGE 8 *************************** */
+/**
+ * 8) Programa una función que elimine cierto patrón de caracteres de un texto dado, pe.
+ * miFuncion("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz") devolverá  "1, 2, 3, 4 y 5.
+ */
+function Pregunta8(texto="", patron=""){
+    if(!texto){
+        console.warn("Ingresa un Texto");
+    } else if (!patron){
+        console.warn("Ingresa un patron");
+    } else {
+        console.log(texto.replace(new RegExp(patron, "ig"), ""))
+    }
+}
+
+//Pregunta8("xyz1, xyz2, xyz3, xyz4, xyz5", "xyz");
+
+
+/******************** CHALLENGE 9 *************************** */
+/**
+ * 9) Programa una función que obtenga un numero aleatorio entre 501 y 600.
+ */
+
+
+/******************** CHALLENGE 10 *************************** */
+/**
+ * 10) Programa una función que reciba un número y evalúe si es capicúa o no
+ * (que se lee igual en un sentido que en otro), pe. miFuncion(2002) devolverá true.
+ */
+
+
+/******************** CHALLENGE 11 *************************** */
+/**
+ * 11) Programa una función que calcule el factorial de un número
+ * (El factorial de un entero positivo n, se define como el producto de todos los números
+ * enteros positivos desde 1 hasta n), pe. miFuncion(5) devolverá 120.
+ */
